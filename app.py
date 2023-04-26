04832349e68c8b2a97ff9f312c3bf1f6cebb3a39
Karolina Redden <dubajkaro@gmail.com>
2023-04-26 13:09:11 -0300

    add active Flask application context as per https://stackoverflow.com/questions/73961938/flask-sqlalchemy-db-create-all-raises-runtimeerror-working-outside-of-applicat

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 # save this as app.py
 from flask import Flask
 from flask import render_template
+
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 
 load_dotenv('./.flaskenv')
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
 
 if __name__ == '__main__':
     app.run()
@@ -16,3 +22,7 @@ if __name__ == '__main__':
 def index():
     name = 'John Smith'
     return render_template('index.html', name=name)
+
+
+with app.app_context():
+    db.create_all()
