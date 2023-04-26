123ee1be84eacaa685c06e521dac837e4357d732
Karolina Redden <dubajkaro@gmail.com>
2023-04-26 13:11:11 -0300

    add model and have db.sqlite to be created through the shell, used python 3 shell and commands >>> from models import Task >>> from app import db >>> from app import app >>> app.app_context().push() >>> db.create_all() >>> t1 = Task(title='New task1') >>> t2= Task(title='New task2') >>> db.session.add(t1) >>> db.session.add(t2) >>> db.session.commit() >>> t1 <Task id: 1 - New task1 >>> t1.id 1

---
+++ models.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+import app
+
+
+class Task(app.db.Model):
+    id = app.db.Column(app.db.Integer(), primary_key=True)
+    title = app.db.Column(app.db.String(140))
+    date = app.db.Column(app.db.DateTime(), default=datetime.now())
+    completed = app.db.Column(app.db.Boolean(), default=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __repr__(self):
+        return f'<Task id: {self.id} - {self.title}'
