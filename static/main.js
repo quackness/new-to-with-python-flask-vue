e6429ebaf09f6f4a23b0dea62def0f36b34d974f
Karolina Redden <dubajkaro@gmail.com>
2023-04-26 11:15:09 -0300

    add sample data to main js file and render the tasks with a loop, add unique id key

--- static/main.js
+++ static/main.js
@@ -3,7 +3,11 @@ const { createApp } = Vue
 const TaskApp = {
   data() {
     return {
-      task: 'new task'
+      task: 'new task',
+      tasks: [
+        { title: 'One' },
+        { title: 'Two' }
+      ]
     }
   },
   delimiters: ['{', '}']
