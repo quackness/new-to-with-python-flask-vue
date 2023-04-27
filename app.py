bc9aeecfa4b0ace1b3aa36935cf88a64c0fb4f25
Karolina Redden <dubajkaro@gmail.com>
2023-04-27 20:38:04 -0300

    set up delete route in flask

--- app.py
+++ app.py
@@ -45,5 +45,15 @@ def create_task():
     return redirect(url_for('index'))
 
 
+@app.route('/delete', methods=['POST'])
+def delete_task():
+    import models
+    task_id = request.get_json().get('id')
+    task = models.Task.query.filter_by(id=task_id).first()
+    db.session.delete(task)
+    db.session.commit()
+    return jsonify({'result': 'ok'}), 200
+
+
 with app.app_context():
     db.create_all()
